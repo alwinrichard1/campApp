021b71590b120f21e21732de4c9933a37348423c
Alwin <alwin.dennis@ust-global.com>
2018-08-18 13:53:55 +0530

    AMD install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { materialImports } from './app.materials.module';
@@ -12,7 +13,8 @@ import { AppRoutingModule } from './app.routing.module';
   imports: [
     BrowserModule,
     materialImports,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
